2df289917d13098b8a13ec4bd6ebaf5bd6668cfe
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-20 19:27:59 +0900

    update　edit　追加

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,7 @@
 class ListController < ApplicationController
+  before_action :setlist, only: %i(edit update)
+
+
   def new 
     @list = List.new
   end
@@ -12,9 +15,26 @@ class ListController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @list.update_attributes(list_params) #update_attributesメソッドは属性のハッシュを受け取り、成功時には更新と保存を続けて同時に行う。
+      redirect_to :root
+    else 
+      render action: :edit
+    end
+  end
+
+
+
   private
    def list_params
     params.require(:list).permit(:title).merge(user: current_user)
+   end
+
+   def set_list
+    @list = List.find_by(id:params[:id])
    end
    
 end
